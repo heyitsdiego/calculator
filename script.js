e52062450b1c3168c4c5264d60af5007e5924d29
Diego <dcoaguila@gmail.com>
2020-06-30 23:39:22 -0700

    refactor: Ran eslint and prettier

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const calculator = {
   operantA: "",
   operator: "",
   operantB: "",
-  
+
   appendNumber(number) {
     if (number == "." && currentInput.value.includes(".")) return;
-    currentInput.value = currentInput.value + number; //posible update display
+    currentInput.value = currentInput.value + number;
   },
 
   updateDisplay() {
     previousInput.innerText = calculator.operantA;
-    currentInput.value = calculator.operantB
+    currentInput.value = calculator.operantB;
   },
 
   initializeExpression(operand) {
@@ -20,12 +20,11 @@ const calculator = {
       calculator.operantA = comptutedExpression;
       calculator.operator = operand;
       calculator.operantB = "";
+    } else {
+      calculator.operantA = currentInput.value;
+      calculator.operator = operand;
     }
-    else {
-    calculator.operantA = currentInput.value;
-    calculator.operator = operand;
-    }
-    calculator.updateDisplay()
+    calculator.updateDisplay();
   },
 
   delete() {
@@ -36,38 +35,40 @@ const calculator = {
     calculator.operantA = "";
     calculator.operantB = "";
     calculator.operator = "";
-    calculator.updateDisplay()
+    calculator.updateDisplay();
   },
 
   equals() {
-    if (calculator.operantA === "" || calculator.operantA === "") return
+    if (calculator.operantA === "" || calculator.operantA === "") return;
     const comptutedExpression = calculator.operate();
     calculator.allClear();
     currentInput.value = comptutedExpression;
   },
   plusMinus() {
     if (currentInput.value.includes("-")) {
-      currentInput.value = currentInput.value.slice(1, currentInput.value.lenght);;
-    }
-    else {
+      currentInput.value = currentInput.value.slice(
+        1,
+        currentInput.value.lenght
+      );
+    } else {
       currentInput.value = "-" + currentInput.value;
     }
   },
 
   operate() {
     calculator.operantB = currentInput.value;
-    switch(calculator.operator) {
-    case "+":
-      return (Number(calculator.operantA) + Number(calculator.operantB));
-    case "-":
-      return Number(calculator.operantA) - Number(calculator.operantB);
-    case "*":
-      return Number(calculator.operantA) * Number(calculator.operantB);
-    case "÷":
-      return Number(calculator.operantA) / Number(calculator.operantB);
-   }
+    switch (calculator.operator) {
+      case "+":
+        return Number(calculator.operantA) + Number(calculator.operantB);
+      case "-":
+        return Number(calculator.operantA) - Number(calculator.operantB);
+      case "*":
+        return Number(calculator.operantA) * Number(calculator.operantB);
+      case "÷":
+        return Number(calculator.operantA) / Number(calculator.operantB);
+    }
   },
-}
+};
 
 const operationButtons = document.querySelectorAll("[data-operation]");
 const numberButtons = document.querySelectorAll("[data-number]");
@@ -75,20 +76,20 @@ const operateButton = document.querySelector("[data-operate]");
 const allClearButton = document.querySelector("[data-all-clear]");
 const deleteButton = document.querySelector("[data-delete]");
 const currentInput = document.getElementById("current-input");
-const previousInput = document.querySelector(".previous-input")
-const plusMinus = document.querySelector("[data-plus-minus]")
+const previousInput = document.querySelector(".previous-input");
+const plusMinus = document.querySelector("[data-plus-minus]");
 
-numberButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    calculator.appendNumber(button.innerText)
-  })
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+  });
 });
-operationButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    calculator.initializeExpression(button.innerText)
-  })
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.initializeExpression(button.innerText);
+  });
 });
-operateButton.addEventListener('click', calculator.equals);
-allClearButton.addEventListener('click', calculator.allClear);
-deleteButton.addEventListener('click', calculator.delete);
-plusMinus.addEventListener('click', calculator.plusMinus);
+operateButton.addEventListener("click", calculator.equals);
+allClearButton.addEventListener("click", calculator.allClear);
+deleteButton.addEventListener("click", calculator.delete);
+plusMinus.addEventListener("click", calculator.plusMinus);
